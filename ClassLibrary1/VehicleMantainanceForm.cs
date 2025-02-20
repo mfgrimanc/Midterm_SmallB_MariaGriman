dd4832d76189547d68a58847618dea39a6ab5b3c
Maria <mfgriman@gmail.com>
2025-02-20 19:37:55 -0400

    Running test

--- ClassLibrary1/VehicleMantainanceForm.cs
+++ ClassLibrary1/VehicleMantainanceForm.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ClassLibrary1
 {
     public class VehicleMantainanceForm
--- TestMidterm/TestVehicleMaintainance.cs
+++ TestMidterm/TestVehicleMaintainance.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using ClassLibrary1;
+using static ClassLibrary1.VehicleMantainanceForm;
+using System.Reflection;
+using System.Management.Instrumentation;
+
+
 
 namespace TestMidterm
 {
+    [TestFixture]
     public class TestVehicleMaintainance
     {
+        //valid model
+      
+        [Test]
+        public void CheckModel_Corolla_ValidNumberOfChar()
+        {
+            // Arrange
+            int numberChar;
+            Vehicle vehicle = new Vehicle("Corolla", 2025, 10000);
+
+            string model = vehicle.Model;
+
+            // Act
+            numberChar = model.Length;
+
+
+            // Assert
+            Assert.That(numberChar, Is.LessThan(15));
+
+        }
     }
 }
