5358138514d1639982e64287730b37a152d66118
Maria <mfgriman@gmail.com>
2025-02-20 17:18:20 -0400

    Initial commit

---
+++ ClassLibrary1/VehicleMantainanceForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class VehicleMantainanceForm
+    {
+        public class Vehicle
+        //BVA Ranges
+        //Model = Maximum 15 characters
+        //Year = 1990-2025
+        //Kilometers = 0-250000
+        {
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public int Kilometers { get; set; }
+
+            public Vehicle(string model, int year, int kilometers)
+            {
+                Model = model;
+                Year = year;
+                Kilometers = kilometers;
+            }
+
+            public string GetModel()
+            {
+                return Model;
+            }
+
+            public int GetYear()
+            {
+                return Year;
+            }
+
+            public int GetKilometers()
+            {
+                return Kilometers;
+            }
+        }
+    }
+}
